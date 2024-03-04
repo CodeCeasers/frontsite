65f8398f93437bce1e648a3b8775d814e89e5cb1
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-04 20:21:09 +0530

    editor

---
+++ client/src/components/VideoChat.jsx
@@ -0,0 +1,13 @@
+export function Video(){
+
+    return <>
+    <div className="flex flex-row">
+        <div className="w-full">
+            video-1
+        </div>
+        <div className="w-full">
+            video-2
+        </div>
+    </div>
+    </>
+}
