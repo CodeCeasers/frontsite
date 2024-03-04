6994b94935617b16062c4e89bbf14e189c4bea44
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2024-03-05 01:13:42 +0530

    Refactor login page component

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -3,16 +3,23 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-// import { Toaster, toast } from "sonner";
-// require("dotenv").config();
+import { Toaster, toast } from "sonner";
+require("dotenv").config();
 
 export default function Login() {
-  //   const router = useRouter();
-  const [form, setForm] = useState({ email: "", password: "" });
+  const router = useRouter();
   const [isCandidate, setCandidate] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    role: `${isCandidate ? "candidate" : "interviewer"}`,
+  });
   const [interviewStyle, setInterviewStyle] = useState(true);
   const [candidateStyle, setCandidateStyle] = useState(false);
-  //   const [theme, setTheme] = useState(true);
+  const [focus, setFocus] = useState({
+    email: false,
+    password: false,
+  });
 
   const toggleCandidate = (isCandid) => () => {
     setCandidate(isCandid);
@@ -41,65 +48,73 @@ export default function Login() {
   //     }
   //   }, []); // Empty dependency array as it runs once on mount
 
-  //   const handleSubmit = async (e) => {
-  //     try {
-  //       e.preventDefault();
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
 
-  //       if (!form.email || !form.password) {
-  //         toast.warning("Please fill in all fields");
-  //         return;
-  //       }
+      if (!form.email || !form.password) {
+        toast.warning("Please fill in all fields");
+        return;
+      }
 
-  //       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}user/login`, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify({
-  //           email: form.email,
-  //           password: form.password,
-  //         }),
-  //       });
+      console.log(form);
 
-  //       const data = await res.json();
+      const res = await fetch(`http://localhost:400/user/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: form.email,
+          password: form.password,
+          role: form.role,
+        }),
+      });
 
-  //       if (data.token) {
-  //         toast.success("Logged in successfully");
-  //         const user = {
-  //           token: data.token,
-  //           id: data.id,
-  //           username: data.username,
-  //           email: form.email,
-  //           rating: data.ratings,
-  //         };
+      const data = await res.json();
 
-  //         if (localStorage.getItem("user")) {
-  //           localStorage.removeItem("user");
-  //         }
+      if (data.token) {
+        toast.success("Logged in successfully");
+        const user = {
+          token: data.token,
+          id: data.id,
+          name: data.name,
+          email: form.email,
+        };
 
-  //         localStorage.setItem("user", JSON.stringify(user));
+        if (localStorage.getItem("candidate")) {
+          localStorage.removeItem("candidate");
+        }
+        if (localStorage.getItem("interviewer")) {
+          localStorage.removeItem("interviewer");
+        }
 
-  //         setForm({ email: "", password: "" });
+        localStorage.setItem(
+          `${form.role == "candidate" ? "candidate" : "interviewer"}`,
+          JSON.stringify(user)
+        );
 
-  //         router.push("/");
-  //       } else {
-  //         toast.error(data.msg);
-  //       }
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
+        setForm({ email: "", password: "" });
+
+        router.push("/");
+      } else {
+        toast.error(data.msg);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <main className="flex flex-col justify-center items-center h-dvh w-dvw bg-[#12151c]">
       <h1 className=" text-center mb-4 text-[2rem] text-white">Log In</h1>
-      <div className="w-[500px] shadow-[0px_20px_50px_0px_rgba(0,0,0,0.25)] pb-[50px] rounded-[10px] bg-slate-600">
+      <div className="w-[500px] shadow-[0px_20px_50px_0px_rgba(0,0,0,0.25)] pb-[50px] rounded-[10px] bg-slate-600 text-white">
         <div className="flex flex-row w-full h-14 mb-14 cursor-pointer">
           <div
             className={`flex justify-center items-center w-2/4 rounded-tl-[10px] ${
               candidateStyle
-                ? `bg-neutral-500 border-b-2 border-transparent`
-                : `bg-gray-500 border-b-2 border-b-white`
+                ? `bg-[#1b222c] border-b-2 border-transparent`
+                : `bg-none border-b-2 border-b-white`
             }`}
             onClick={toggleCandidate(true)}
           >
@@ -108,8 +123,8 @@ export default function Login() {
           <div
             className={`flex justify-center items-center w-2/4 rounded-tr-[10px]  ${
               interviewStyle
-                ? `bg-neutral-500 border-b-2 border-transparent`
-                : `bg-gray-500 border-b-2 border-b-white`
+                ? `bg-[#1b222c] border-b-2 border-transparent`
+                : `bg-none border-b-2 border-b-white`
             }`}
             onClick={toggleCandidate(false)}
           >
@@ -117,49 +132,59 @@ export default function Login() {
           </div>
         </div>
         <form className="flex flex-col mb-8 px-[50px]">
-          <label className="flex flex-col gap-[0.5em] mb-[2em]">
+          <label
+            className={`flex flex-col gap-[0.5em] mb-[2em] ${
+              focus.email ? "underline" : ""
+            }`}
+          >
             <div className="text-[0.8em]">EMAIL ADDRESS</div>
             <input
-              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
               type="email"
               name="email"
               required
               onChange={(e) => {
-                -setForm({ ...form, email: e.target.value });
+                setForm({ ...form, email: e.target.value });
               }}
+              onFocus={() => setFocus({ ...focus, email: true })}
+              onBlur={() => setFocus({ ...focus, email: false })}
               value={form.email}
             />
           </label>
-          <label className="flex flex-col gap-[0.5em] mb-[2em]">
+          <label
+            className={`flex flex-col gap-[0.5em] mb-[2em] ${
+              focus.password ? "underline" : ""
+            }`}
+          >
             <div className="text-[0.8em]">PASSWORD</div>
             <input
-              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
               type="password"
               name="password"
               required
               onChange={(e) => {
                 setForm({ ...form, password: e.target.value });
               }}
+              onFocus={() => setFocus({ ...focus, password: true })}
+              onBlur={() => setFocus({ ...focus, password: false })}
               value={form.password}
             />
           </label>
-          <div className="">
-            <Link href="/forgot_password">Forgot password?</Link>
-          </div>
           <input
-            className="text-[19px] text-white cursor-pointer mt-2.5 p-2.5 rounded-[5px] border-[none] bg-slate-950"
+            className="text-[19px] text-white cursor-pointer mt-2.5 p-2.5 rounded-[5px] border-[none] bg-slate-950 font-bold"
             type="submit"
             value="Log In"
+            onClick={handleSubmit}
           />
         </form>
         <div className="flex flex-row justify-between px-[50px]">
           <div>Don't have an account?</div>
-          <div className="underline">
+          <div className="underline font-semibold">
             <Link href="/signup">Sign up</Link>
           </div>
         </div>
       </div>
-      {/* <Toaster theme={theme ? "dark" : "light"} position="bottom-left" /> */}
+      <Toaster theme="light" position="bottom-left" />
     </main>
   );
 }
--- client/src/app/signup/page.jsx
+++ client/src/app/signup/page.jsx
@@ -7,7 +7,7 @@ import { Toaster, toast } from "sonner";
 require("dotenv").config();
 
 export default function SignUp() {
-  //   const router = useRouter();
+  const router = useRouter();
   const [isCandidate, setCandidate] = useState(false);
   const [form, setForm] = useState({
     email: "",
@@ -18,6 +18,12 @@ export default function SignUp() {
   });
   const [interviewStyle, setInterviewStyle] = useState(true);
   const [candidateStyle, setCandidateStyle] = useState(false);
+  const [focus, setFocus] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    password: false,
+  });
 
   const toggleCandidate = (isCandid) => () => {
     setCandidate(isCandid);
@@ -55,6 +61,8 @@ export default function SignUp() {
         return;
       }
 
+      console.log(form);
+
       const res = await fetch(`http://localhost:400/user/login`, {
         method: "POST",
         headers: {
@@ -62,9 +70,9 @@ export default function SignUp() {
         },
         body: JSON.stringify({
           email: form.email,
-          firstName: form.firstName,
-          lastName: form.lastName,
+          name: `${form.firstName} ${form.lastName}`,
           password: form.password,
+          role: form.role,
         }),
       });
 
@@ -76,17 +84,22 @@ export default function SignUp() {
           token: data.token,
           id: data.id,
           email: form.email,
-          firstName: form.firstName,
-          lastName: form.lastName,
+          name: `${form.firstName} ${form.lastName}`,
         };
 
-        if (localStorage.getItem("user")) {
-          localStorage.removeItem("user");
+        if (localStorage.getItem("candidate")) {
+          localStorage.removeItem("candidate");
+        }
+        if (localStorage.getItem("interviewer")) {
+          localStorage.removeItem("interviewer");
         }
 
-        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem(
+          `${form.role == "candidate" ? "candidate" : "interviewer"}`,
+          JSON.stringify(user)
+        );
 
-        setForm({ email: "", password: "" });
+        setForm({ email: "", password: "", firstName: "", lastName: "" });
 
         router.push("/");
       } else {
@@ -100,23 +113,23 @@ export default function SignUp() {
   return (
     <div className="flex flex-col justify-center items-center h-dvh w-dvw bg-[#12151c]">
       <h1 className=" text-center mb-4 text-[2rem] text-white">Sign Up</h1>
-      <div className="w-[650px] shadow-[0px_20px_50px_0px_rgba(0,0,0,0.25)] pb-[50px] rounded-[10px] bg-slate-600">
+      <div className="w-[650px] shadow-[0px_20px_50px_0px_rgba(0,0,0,0.25)] pb-[50px] rounded-[15px] bg-slate-600 text-white">
         <div className="flex flex-row w-full h-14 mb-12 cursor-pointer">
           <div
-            className={`flex justify-center items-center w-2/4 rounded-tl-[10px] ${
+            className={`flex justify-center items-center w-2/4 rounded-tl-[15px] ${
               candidateStyle
-                ? `bg-neutral-500 border-b-2 border-transparent`
-                : `bg-gray-500 border-b-2 border-b-white`
+                ? `bg-[#1b222c] border-b-2 border-transparent`
+                : `bg-none border-b-2 border-b-white`
             }`}
             onClick={toggleCandidate(true)}
           >
             <span>Candidate</span>
           </div>
           <div
-            className={`flex justify-center items-center w-2/4 rounded-tr-[10px]  ${
+            className={`flex justify-center items-center w-2/4 rounded-tr-[15px]  ${
               interviewStyle
-                ? `bg-neutral-500 border-b-2 border-transparent`
-                : `bg-gray-500 border-b-2 border-b-white`
+                ? `bg-[#1b222c] border-b-2 border-transparent`
+                : `bg-none border-b-2 border-b-white`
             }`}
             onClick={toggleCandidate(false)}
           >
@@ -125,64 +138,79 @@ export default function SignUp() {
         </div>
         <form className="flex flex-col mb-8 px-[50px]">
           <div className="flex flex-row justify-between">
-            <label className="flex flex-col gap-[0.5em] mb-[2em]">
+            <label
+              className={`flex flex-col gap-[0.5em] mb-[2em] ${
+                focus.firstName ? "underline" : ""
+              }`}
+            >
               <div className="text-[0.8em]">FIRST NAME</div>
               <input
-                className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+                className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
                 type="text"
                 name="firstName"
                 required
-                onChange={(e) => {
-                  -setForm({ ...form, firstName: e.target.value });
-                }}
+                onChange={(e) =>
+                  setForm({ ...form, firstName: e.target.value })
+                }
+                onFocus={() => setFocus({ ...focus, firstName: true })}
+                onBlur={() => setFocus({ ...focus, firstName: false })}
                 value={form.firstName}
               />
             </label>
-            <label className="flex flex-col gap-[0.5em] mb-[2em]">
+            <label
+              className={`flex flex-col gap-[0.5em] mb-[2em] ${
+                focus.lastName ? "underline" : ""
+              }`}
+            >
               <div className="text-[0.8em]">LAST NAME</div>
               <input
-                className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+                className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
                 type="text"
                 name="lastName"
                 required
-                onChange={(e) => {
-                  -setForm({ ...form, lastName: e.target.value });
-                }}
+                onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+                onFocus={() => setFocus({ ...focus, lastName: true })}
+                onBlur={() => setFocus({ ...focus, lastName: false })}
                 value={form.lastName}
               />
             </label>
           </div>
-          <label className="flex flex-col gap-[0.5em] mb-[2em]">
+          <label
+            className={`flex flex-col gap-[0.5em] mb-[2em] ${
+              focus.email ? "underline" : ""
+            }`}
+          >
             <div className="text-[0.8em]">EMAIL ADDRESS</div>
             <input
-              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
               type="email"
               name="email"
               required
-              onChange={(e) => {
-                setForm({ ...form, email: e.target.value });
-              }}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              onFocus={() => setFocus({ ...focus, email: true })}
+              onBlur={() => setFocus({ ...focus, email: false })}
               value={form.email}
             />
           </label>
-          <label className="flex flex-col gap-[0.5em] mb-[2em]">
+          <label
+            className={`flex flex-col gap-[0.5em] mb-[2em] ${
+              focus.password ? "underline" : ""
+            }`}
+          >
             <div className="text-[0.8em]">PASSWORD</div>
             <input
-              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black"
+              className="text-[1em] border px-[15px] py-2.5 rounded-[5px] border-solid border-[#ddd] text-black outline-none"
               type="password"
               name="password"
               required
-              onChange={(e) => {
-                setForm({ ...form, password: e.target.value });
-              }}
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              onFocus={() => setFocus({ ...focus, password: true })}
+              onBlur={() => setFocus({ ...focus, password: false })}
               value={form.password}
             />
           </label>
-          <div className="">
-            <Link href="/forgot_password">Forgot password?</Link>
-          </div>
           <input
-            className="text-[19px] text-white cursor-pointer mt-2.5 p-2.5 rounded-[5px] border-[none] bg-slate-950"
+            className="text-[19px] text-white cursor-pointer mt-2.5 p-2.5 rounded-[5px] border-[none] bg-slate-950 font-bold"
             type="submit"
             value="Sign Up"
             onClick={handleSubmit}
@@ -190,7 +218,7 @@ export default function SignUp() {
         </form>
         <div className="flex flex-row justify-between px-[50px]">
           <div>Already have an account?</div>
-          <div className=" underline">
+          <div className=" underline font-semibold">
             <Link href="/login">Login</Link>
           </div>
         </div>
